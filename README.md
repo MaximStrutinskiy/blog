ff2aa8a75b42b26c4793b8375824920abf96c590
MaximStrutinskiy <max.strutinskiy>
2016-12-05 02:56:52 +0200

    add index-header section

---
+++ README.md
@@ -0,0 +1,37 @@
+**GeekHub - Blog:**
+
+---
+
+**Install project**
+```
+- Create DB, and add settings for connected;
+- $composer install;
+- $npm install;
+```
+
+---
+
+
+**Use commands:**
+```
+- $php bin/symfony_requirements - check project settings.
+- $php bin/console generate:bundle - create bundle.
+```
+---
+
+**Use materials:** 
+```
+0) GeekHub materials - https://plus.google.com/u/0/communities/103356895682698281466
+1) Http fundamentals - http://symfony.com/doc/current/introduction/http_fundamentals.html  
+2) Architecture - http://symfony.com/doc/current/quick_tour/the_architecture.html
+3) Configuration - http://symfony.com/doc/current/configuration.html
+4) Bundles - http://symfony.com/doc/current/bundles.html 
+5) Routing - http://symfony.com/doc/current/routing.html
+6) Controller - https://symfony.com/doc/current/controller.html
+7) External resources - http://symfony.com/doc/current/routing/external_resources.html
+8) Http foundation - http://symfony.com/doc/current/components/http_foundation.html
+9) Sessions - https://symfony.com/doc/current/components/http_foundation/sessions.html
+10) Setting cookies - http://symfony.com/doc/current/components/http_foundation.html#setting-cookies
+11) Redirect in config - http://symfony.com/doc/current/routing/redirect_in_config.html
+12) Twig - http://twig.sensiolabs.org/
+```
--- web/assets/src/js/main.js
+++ web/assets/src/js/main.js
@@ -1,2 +1,55 @@
 $(document).ready(function(){
+
+    //Function - Index homepage min-height
+    function minHeight(tag){
+        $hwd = $(window).height(); //height screen device
+        $sh = $(tag);              //section bark need min-height
+        $sh.css('min-height',$hwd);
+    }
+
+    //Toogle menu button
+    function toogleButton() {
+        $toogle = $('#toogle');
+        $toogleDiv = $('.mobile-bg');
+
+        $toogle.on('click', function(){
+
+            if(this.classList.contains('active')){
+                this.classList.remove('active');
+
+                $toogleDiv.remove();
+            }else {
+                this.classList.add('active');
+
+                //add mobile menu
+                $div = '<div class="mobile-bg"></div>';
+                $('body').prepend($div);
+            }
+        });
+
+        $toogleDiv.on('click', function(){
+            $toogleDiv.remove();
+            $toogle.classList.remove('active');
+        });
+
+    }
+
+    // $('.mobile-bg').on('click', function(){
+    //     $('#toogle').removeClass('active');
+    //     $('.mobile-bg').remove();
+    // });
+
+    //Run functions
+    minHeight('#index-header');
+    toogleButton();
+
+
+
+    $(window).resize(function () {
+        minHeight('#index-header');
+        $('#toogle').removeClass('active');
+        $('.mobile-bg').remove();
+    });
+
+
 });
